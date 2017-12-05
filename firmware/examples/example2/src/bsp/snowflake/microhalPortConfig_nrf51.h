ca4f7a67bd192c5a926a5d34d8bdff92c5012b9a
buleks <>
2017-12-05 00:52:28 +0000

    First example prepared

--- firmware/examples/example2/src/bsp/snowflake/microhalPortConfig_nrf51.h
+++ firmware/examples/example2/src/bsp/snowflake/microhalPortConfig_nrf51.h
@@ -2,13 +2,13 @@
  * @license    BSD 3-Clause
  * @copyright  microHAL
  * @version    $Id$
- * @brief      Snowflake main file.
+ * @brief      
  *
- * @authors    Pawel Okas, buleks
- * created on: 16-11-2017
- * last modification: 16-11-2017
+ * @authors    pawel
+ * created on: 27-12-2016
+ * last modification: 27-12-2016
  *
- * @copyright Copyright (c) 2017, Pawel Okas
+ * @copyright Copyright (c) 2016, microHAL
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -27,37 +27,16 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "bsp.h"
-#include "microhal.h"
-#include "ports/nrf51/timer.h"
+#ifndef _MICROHAL_MICROHALPORTCONFIG_NRF51_H_
+#define _MICROHAL_MICROHALPORTCONFIG_NRF51_H_
+/* **************************************************************************************************************************************************
+ * INCLUDES
+ */
 
-using namespace microhal;
-using namespace std::literals::chrono_literals;
 
-nrf51::Timer timer(*NRF_TIMER0);
+/* **************************************************************************************************************************************************
+ * CLASS
+ */
 
-class RGB {
- public:
-    RGB(GPIO::IOPin ledr_pin, GPIO::IOPin ledg_pin, GPIO::IOPin ledb_pin)
-        : r(ledr_pin, GPIO::Direction::Output), g(ledg_pin, GPIO::Direction::Output), b(ledb_pin, GPIO::Direction::Output) {}
 
-    GPIO r;
-    GPIO g;
-    GPIO b;
-};
-
-RGB ledTop(ledAR_pin, ledAG_pin, ledAB_pin);
-RGB ledLeftBottom(ledBR_pin, ledBG_pin, ledBB_pin);
-RGB ledRightBottom(ledCR_pin, ledCG_pin, ledCB_pin);
-RGB ledRightTop(ledDR_pin, ledDG_pin, ledDB_pin);
-RGB ledBottom(ledER_pin, ledEG_pin, ledEB_pin);
-RGB ledLeftTop(ledFR_pin, ledFG_pin, ledFB_pin);
-
-int main(void) {
-    while (1) {
-        std::this_thread::sleep_for(20ms);
-        ledTop.g.toggle();
-    }
-
-    return 0;
-}
+#endif  // _MICROHAL_MICROHALPORTCONFIG_NRF51_H_
